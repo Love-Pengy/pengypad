e751740d308ce4435f9257e2c0dd9f1a62779d80
Love-Pengy <afrazierboy2001@gmail.com>
2024-07-09 10:55:20 -0400

    [FEAT] change fade to fade down

--- ws2812/ws2812.c
+++ ws2812/ws2812.c
@@ -14,9 +14,15 @@
 
 #define IS_RGBW true
 #define NUM_PIXELS 1
-
 #define WS2812_PIN 16
 static bool PAUSE_INDICATOR = false;
+// start going up
+static int dir = 1;
+
+enum{
+    ENFORCE = 0, 
+    SIDEEYE, 
+};
 
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -29,50 +35,30 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
 void pattern_fade(uint len, uint t) {
     (void)len;
     (void)t;
-    int max = 50;  // let's not draw too much current!
+    int max = 35;  
     t %= max;
 
     uint32_t val;
+    int zeroEnforcer = SIDEEYE;
     for (int i = 0; i < len; ++i) {
         val = (t * 0x10101) > (0x10101 * 10) ? (0x10101 * 10) : (t * 0x10101);
-        if(!val){
-            PAUSE_INDICATOR = true;
+        if (!val || (++t >= max)) {
+            switch (dir) {
+                case 1:
+                    dir = -1;
+                    break;
+                case -1:
+                    dir = 1;
+                    zeroEnforcer = ENFORCE;
+                    put_pixel(val);
+                    sleep_ms(500);
+                    break;
+            }
         }
-        put_pixel(val);
-        if (++t >= max) t = 0;
-    }
-}
-
-void pattern_snakes(uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(0);
-    }
-}
-
-void pattern_random(uint len, uint t) {
-    if (t % 8) return;
-    for (int i = 0; i < len; ++i) put_pixel(rand());
-}
-
-void pattern_sparkle(uint len, uint t) {
-    if (t % 8) return;
-    for (int i = 0; i < len; ++i) put_pixel(rand() % 16 ? 0 : 0xffffffff);
-}
-
-void pattern_greys(uint len, uint t) {
-    int max = 100;  // let's not draw too much current!
-    t %= max;
-    for (int i = 0; i < len; ++i) {
-        put_pixel(t * 0x10101);
-        if (++t >= max) t = 0;
+        if(zeroEnforcer){
+            put_pixel(val);
+        }
+        //if (++t >= max){ t = 0; printf("MAXX\n");}
     }
 }
 
@@ -81,10 +67,6 @@ const struct {
     pattern pat;
     const char* name;
 } pattern_table[] = {
-    //{pattern_snakes,  "Snakes!"},
-    //{pattern_random,  "Random data"},
-    //{pattern_sparkle, "Sparkles"},
-    //{pattern_greys, "Greys"},
     {pattern_fade, "Fade"},
 };
 
@@ -100,23 +82,16 @@ int main() {
 
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    int t = 0;
+    int t = 1;
     while (1) {
-        int pat = rand() % count_of(pattern_table);
-        /*int dir = (rand() >> 30) & 1 ? 1 : -1;*/
-        int dir = -1;
-        // puts(pattern_table[pat].name);
-        // puts(dir == 1 ? "(forward)" : "(backward)");
+        int pat = 0;
         for (int i = 0; i < 1000; ++i) {
-            if(PAUSE_INDICATOR){
-                PAUSE_INDICATOR = false;
-                sleep_ms(500);
-            }
             pattern_table[pat].pat(NUM_PIXELS, t);
             sleep_ms(10);
             if (!(i % 5)) {
                 t += dir;
             }
         }
+        //sleep_ms(2000);
     }
 }
