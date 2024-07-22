4264054ba312d12413cb4b7432ff0d2b155a7f01
Love-Pengy <afrazierboy2001@gmail.com>
2024-07-22 19:14:49 -0400

    [DEBUG] fix for encoder detection

--- encoder/encoder.c
+++ encoder/encoder.c
@@ -24,89 +24,53 @@ void initEncoder(void) {
 int main() {
     stdio_init_all();
     initEncoder();
-    // 0x00 = both low
-    // 0x01 = B high
-    // 0x10 = A high
-    // 0x11 = perform action
     static uint8_t state = 0;
-    // 0 = none
-    // 1 = A side
-    // 2 = B side
-    /*
-     * check which one goes high first
-     * send key for direction
-     * wait for the opposite one to go low
-     * once that happens reset
-     * */
     static uint8_t dir = 0;
-    // TODO: fix both being registered on each tick. The direction is right
-    // already
-    static bool a = false;
-    static bool b = false;
-    static bool needReset = false;
+    static uint8_t last = 0;
+    static bool reset = false;
 
     while (true) {
-        //printf("TEST: %d %d\n", needReset, state);
-        if (!state) {
-            a = false;
-            b = false;
-            gpio_put(8, 1);
-            sleep_us(50);
-            if (gpio_get(9)) {
-                a = true;
-                gpio_put(8, 0);
-            }
-            else {
-                gpio_put(10, 1);
-                sleep_us(50);
-                if (gpio_get(9)) {
-                    b = true;
-                    gpio_put(10, 0);
-                }
-            }
-            if (!needReset) {
-                if (a ^ b) {
-                    if (a) {
-                        state |= 0x01;
-                    }
-                    else {
-                        state |= 0x10;
-                    }
-                }
-            }
-            else {
-                if (!a && !b) {
-                    needReset = false;
-                }
-            }
+        state &= 0x00;
+        gpio_put(8, 1);
+        sleep_us(50);
+        if (gpio_get(9)) {
+            state |= 0x01;
         }
-        else {
+        gpio_put(8, 0);
+        gpio_put(10, 1);
+        sleep_us(50);
+        if (gpio_get(9)) {
+            state |= 0x10;
+        }
+        gpio_put(10, 0);
+        if (!dir) {
             switch (state) {
                 case 0x01:
-                    gpio_put(10, 1);
-                    sleep_us(50);
-                    if (gpio_get(9) && !needReset) {
-                        printf("SEND 8->10 KEY\n");
-                        needReset = true;
-                        state = 0x00;
-                    }
-                    gpio_put(10, 0);
-                    sleep_us(50);
+                    dir = 0x01;
+                    last = 0x10;
                     break;
                 case 0x10:
-                    gpio_put(8, 1);
-                    sleep_us(50);
-                    if (gpio_get(9) && !needReset) {
-                        printf("SEND 10->8 KEY\n");
-                        needReset = true;
-                        state = 0x00;
-                    }
-                    gpio_put(8, 0);
-                    sleep_us(50);
+                    dir = 0x10;
+                    last = 0x01;
                     break;
                 default:
                     break;
             }
         }
+        else {
+            if (!reset) {
+                if (state == last) {
+                    reset = true;
+                }
+            }
+            else {
+                if (!state && reset) {
+                    printf("SEND: %d\n", dir);
+                    dir = 0x00;
+                    reset = false;
+                }
+            }
+        }
     }
 }
+
